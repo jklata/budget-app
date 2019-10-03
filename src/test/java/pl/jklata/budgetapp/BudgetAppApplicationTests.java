dc942da8d8004bcf9080a3ea5a4ea6c674fb1842
Kuba - PC <kuba4747@gmail.com>
2019-10-03 13:07:48 +0200

    init commit

---
+++ src/test/java/pl/jklata/budgetapp/BudgetAppApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.jklata.budgetapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BudgetAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
