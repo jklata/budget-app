db4327b473fa5c31ed8efb0bc61e722ef89a9a43
Kuba - PC <kuba4747@gmail.com>
2019-10-08 12:45:51 +0200

    DataInitializer refactor. Templates refactor.

--- src/main/java/pl/jklata/budgetapp/controller/DataInitializer.java
+++ src/main/java/pl/jklata/budgetapp/controller/DataInitializer.java
@@ -59,43 +59,42 @@ public class DataInitializer implements ApplicationListener<ContextRefreshedEven
         tranCat1.setName("Samochód");
         tranCat1.setColor("#0040FF");
 
+        TransactionCategory tranCat2 = new TransactionCategory();
+        tranCat2.setName("Dom");
+        tranCat2.setColor("#e83e8c");
+
+        TransactionCategory tranCat3 = new TransactionCategory();
+        tranCat3.setName("Jedzenie");
+        tranCat3.setColor("#e83e8c");
+
+        List<TransactionCategory> transactionCategories = new ArrayList<>(Arrays.asList(tranCat1, tranCat2, tranCat3));
+
         Hashtag ht1 = new Hashtag();
         ht1.setName("orlen");
         Hashtag ht2 = new Hashtag();
         ht2.setName("samochód");
 
 
-        Transaction transaction1 = new Transaction();
-        transaction1.setTransactionDate(LocalDate.of(2010,4,13));
-        transaction1.setInsertDate(LocalDate.now());
-        transaction1.setAmount(new BigDecimal(253.12));
-        transaction1.setPayee("ORLEN paliwo");
-        transaction1.setTransactionCategory(tranCat1);
-        transaction1.setTransactionType(TransactionType.EXPENSE);
-        transaction1.setAccount(account1);
-        transaction1.setBudget(budget1);
-        transaction1.setHashtags(new HashSet<>(Arrays.asList(ht1, ht2)));
-
-        Transaction transaction2 = new Transaction();
-        transaction2.setTransactionDate(LocalDate.of(2014,2,27));
-        transaction2.setInsertDate(LocalDate.now());
-        transaction2.setAmount(new BigDecimal(139.42));
-        transaction2.setPayee("SHELL paliwo");
-        transaction2.setTransactionCategory(tranCat1);
-        transaction2.setTransactionType(TransactionType.EXPENSE);
-        transaction2.setAccount(account1);
-        transaction2.setBudget(budget1);
-        transaction2.setHashtags(new HashSet<>(Arrays.asList(ht2)));
-
         userRepository.save(user1);
         accountRepository.save(account1);
-        transactionCategoryRepository.save(tranCat1);
+        transactionCategoryRepository.saveAll(transactionCategories);
         hashtagRepository.saveAll(new HashSet<>(Arrays.asList(ht1, ht2)));
         budgetRepository.save(budget1);
-        transactionRepository.saveAll(new ArrayList<>(Arrays.asList(transaction1, transaction2)));
 
+        Random r = new Random();
+        for (int i = 0; i <15 ; i++) {
 
+            Transaction transaction = new Transaction();
+            transaction.setTransactionDate(LocalDate.of(r.nextInt(2019-2000)+2000,r.nextInt(12-1)+1,r.nextInt(30-1)+1));
+            transaction.setInsertDate(LocalDate.now());
+            transaction.setAmount(new BigDecimal(r.nextInt(3000-100)+100));
+            transaction.setPayee("Odbiorca " + r.nextInt((i+1)+1));
+            transaction.setTransactionCategory(transactionCategories.get(r.nextInt(4-1)));
+            transaction.setTransactionType(TransactionType.EXPENSE);
+            transaction.setAccount(account1);
+            transaction.setBudget(budget1);
+            transaction.setHashtags(new HashSet<>(Arrays.asList(ht2)));
+            transactionRepository.save(transaction);
+        }
     }
-
-
 }
