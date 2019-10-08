d6600dec7234ef3b8f3b676c0d0a9ba49f249fcd
Kuba - PC <kuba4747@gmail.com>
2019-10-08 13:26:22 +0200

    DataInitializer refactor. Templates refactor.

--- src/main/java/pl/jklata/budgetapp/controller/NavController.java
+++ src/main/java/pl/jklata/budgetapp/controller/NavController.java
@@ -1,11 +1,21 @@
 package pl.jklata.budgetapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.jklata.budgetapp.domain.Transaction;
+import pl.jklata.budgetapp.repository.TransactionRepository;
+
+import java.util.List;
 
 @Controller
 public class NavController {
 
+    private TransactionRepository transactionRepository;
+
+    public NavController(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
 
     @RequestMapping({"", "/", "/index"})
     public String getIndex() {
@@ -13,7 +23,10 @@ public class NavController {
     }
 
     @RequestMapping({"/transactions"})
-    public String getTransactions() {
+    public String getTransactions(Model model) {
+
+        model.addAttribute("transactions", transactionRepository.findAll());
+
         return "transactions";
     }
 
