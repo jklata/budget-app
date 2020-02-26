cdaafdcfad2ccde0f1adf53fafe887f483d99f37
Jakub Klata <jakub.klata@pentacomp.pl>
2020-02-26 13:30:50 +0100

    SQL drop all tables script

--- database/drop/drop_all_tables.sql
+++ database/drop/drop_all_tables.sql
@@ -5,5 +5,8 @@ drop table if exists payment;
 drop table if exists budget;
 drop table if exists hashtag;
 drop table if exists payment_has_hashtag;
+drop table if exists user_has_user_role;
+drop table if exists user_role_has_user;
+drop table if exists user_role;
 
 
