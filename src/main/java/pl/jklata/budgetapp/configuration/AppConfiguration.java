4d2975e89da783a83d11743896431bf31866b6bc
Jakub Klata <jakub.klata@pentacomp.pl>
2020-07-13 16:23:15 +0200

    Spring Security integration with H2-console

--- src/main/java/pl/jklata/budgetapp/configuration/AppConfiguration.java
+++ src/main/java/pl/jklata/budgetapp/configuration/AppConfiguration.java
@@ -1,8 +1,10 @@
 package pl.jklata.budgetapp.configuration;
 
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.dialect.springdata.SpringDataDialect;
+import org.h2.server.web.WebServlet;
 
 @Configuration
 public class AppConfiguration {
@@ -10,5 +12,12 @@ public class AppConfiguration {
     @Bean
     public SpringDataDialect springDataDialect() {
         return new SpringDataDialect();
+    }
+
+    @Bean
+    ServletRegistrationBean h2servletRegistration() {
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
+        registrationBean.addUrlMappings("/h2-console/*");
+        return registrationBean;
     }
 }
