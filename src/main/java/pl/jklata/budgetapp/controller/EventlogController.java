ca7e045869519865f2bfb0eda128780fc5d19aef
Kuba - PC <kuba4747@gmail.com>
2020-08-26 15:15:25 +0200

    Prepare eventlog view.

---
+++ src/main/java/pl/jklata/budgetapp/controller/EventlogController.java
@@ -0,0 +1,18 @@
+package pl.jklata.budgetapp.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping(value = "/eventlog")
+public class EventlogController {
+
+    @GetMapping(value = "")
+    public String eventLog(){
+        return "eventlog.html";
+    }
+
+}
